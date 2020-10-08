af83c7fb1690e200b0ae0d59bd7f586ed67dd067
neverperfect <hai_adhibody@yahoo.com.au>
2020-10-08 12:09:31 +0700

    Add files via upload

---
+++ app/models/item.model.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, Sequelize) => {
+    const Item = sequelize.define("item", {
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      }
+    });
+  
+    return Item;
+  };
---
+++ app/routes/item.routes.js
@@ -0,0 +1,22 @@
+module.exports = app => {
+    const items = require("../controllers/item.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new Item
+    router.post("/", items.create);
+  
+    // Retrieve all Items
+    router.get("/", items.findAll);
+  
+    // Retrieve a single Item with id
+    router.get("/:id", items.findOne);
+  
+    // Update a Item with id
+    router.put("/:id", items.update);
+  
+    // Delete a Item with id
+    router.delete("/:id", items.delete);
+  
+    app.use("/api/tutorials", router);
+  };
